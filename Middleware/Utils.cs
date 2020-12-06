537c35c04c12ef2e2e4d4742ecd4162d5f857da9
UnRunDead <me@unrundead.dev>
2020-12-06 17:16:05 +0100

    Created Middleware utils for easier input reading

---
+++ Middleware/Utils.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace Middleware {
+    public static class Utils {
+        public static string[] GetPuzzleInput(string filename) {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzle",$"{filename}.txt");
+            return File.ReadAllLines(filePath);
+        }
+    }
+}
