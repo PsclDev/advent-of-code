879bc3c27b91b2910e286b95450f4a9e6be5ce77
UnRunDead <me@unrundead.dev>
2020-12-06 17:17:13 +0100

    Finished Day 1

---
+++ Year-2020/days/01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Middleware;
+
+namespace Year_2020.days {
+    public static class DayOne{
+        private static int[] GetPuzzleInput() {
+            var input = Utils.GetPuzzleInput("01-1");
+            return Array.ConvertAll(input, i => Int32.Parse(i));
+        }
+        public static void TaskOne() {
+            var input = GetPuzzleInput();
+            foreach (var i in input) {
+                foreach (var k in input) {
+                    if(i + k == 2020)
+                        Console.WriteLine(i * k);
+                }
+            }
+        }
+
+        public static void TaskTwo() {
+            var input = GetPuzzleInput();
+            foreach (var i in input) {
+                foreach (var k in input) {
+                    foreach (var j in input) {
+                        if(i + k + j == 2020)
+                            Console.WriteLine(i * k * j);
+                    }
+                }
+            }
+        }
+    }
+}
